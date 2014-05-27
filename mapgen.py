17f57eb438af0cf1f21ee4327a9ec2657c527e48
Damien Moore <damienlmoore@gmail.com>
2014-05-26 23:36:11 -0400

    move map generation to a separate module (mapgen.py)

---
+++ mapgen.py
@@ -0,0 +1,70 @@
+#std/external libs
+import time
+import numpy
+
+#local libs
+from config import SECTOR_SIZE, SECTOR_HEIGHT, LOADED_SECTORS
+from blocks import BLOCK_VERTICES, BLOCK_COLORS, BLOCK_NORMALS, BLOCK_TEXTURES, BLOCK_ID, BLOCK_SOLID, TEXTURE_PATH
+import noise
+
+STONE = BLOCK_ID['Stone']
+SAND = BLOCK_ID['Sand']
+GRASS = BLOCK_ID['Grass']
+
+
+class SectorNoise2D(object):
+    def __init__(self, seed, step = SECTOR_SIZE, step_offset = 0, scale = 1, offset = 0):
+        self.noise = noise.SimplexNoise(seed = seed)
+        self.seed = seed
+        self.step = step
+        self.scale = scale
+        self.offset = offset
+        Z = numpy.mgrid[0:SECTOR_SIZE,0:SECTOR_SIZE].T
+        shape = Z.shape
+        self.Z = Z.reshape((shape[0]*shape[1],2))+step_offset
+
+    def __call__(self, position):
+        Z = self.Z + numpy.array([position[0],position[2]])
+        N=self.noise.noise(Z/self.step)*self.scale + self.offset
+        return N.reshape((SECTOR_SIZE,SECTOR_SIZE))
+
+def initialize_map_generator():
+    global noise1, noise2, noise3, noise4
+    HILL_STEP = 40.0
+    HILL_SCALE = 5
+    HILL_OFFSET = 5
+    CONTINENTAL_STEP = 1500.0
+    CONTINENTAL_SCALE = 40.0
+    CONTINENTAL_OFFSET = 20
+    GAIN_STEP = 3000.0
+    GAIN_SCALE = 5
+    GAIN_OFFSET = 5
+    seed = int(time.time())
+    noise1 = SectorNoise2D(seed = seed+12, step = HILL_STEP, step_offset = 30,  
+        scale = HILL_SCALE, offset = HILL_OFFSET)
+    noise2 = SectorNoise2D(seed = seed+16, step = HILL_STEP, step_offset = 900,
+        scale = HILL_SCALE, offset = HILL_OFFSET)
+    noise3 = SectorNoise2D(seed = seed+14, step = CONTINENTAL_STEP, step_offset = 531,
+        scale = CONTINENTAL_SCALE, offset = CONTINENTAL_OFFSET)
+    noise4 = SectorNoise2D(seed = seed+18, step = GAIN_STEP, step_offset = 8123,
+        scale = GAIN_SCALE, offset = GAIN_OFFSET)
+
+def generate_sector(position, sector, world):
+    """ Initialize the sector by procedurally generating terrain using
+    simplex noise.
+
+    """
+    N1=noise1(position)
+    N2=noise2(position)
+    N3=noise3(position)
+    N4=noise4(position)
+
+    N1 = N1*N4+N3
+    N2 = N2+N3
+
+    b = numpy.zeros((SECTOR_HEIGHT,SECTOR_SIZE,SECTOR_SIZE),dtype='u2')
+    for y in range(SECTOR_HEIGHT):
+        b[y] = ((y-40<N1-3)*STONE + (((y-40>=N1-3) & (y-40<N1))*GRASS))
+        thresh = ((y-40>N3)*(y-40<N2)*(y>10))>0
+        b[y] = b[y]*(1 - thresh) + SAND * thresh
+    return b.swapaxes(0,1).swapaxes(0,2)
--- world.py
+++ world.py
@@ -15,48 +15,12 @@ import pyglet.gl as gl
 from config import SECTOR_SIZE, SECTOR_HEIGHT, LOADED_SECTORS
 from util import normalize, sectorize, FACES, cube_v, cube_v2
 from blocks import BLOCK_VERTICES, BLOCK_COLORS, BLOCK_NORMALS, BLOCK_TEXTURES, BLOCK_ID, BLOCK_SOLID, TEXTURE_PATH
-import noise
+import mapgen
 
 SECTOR_GRID = numpy.mgrid[:SECTOR_SIZE,:SECTOR_HEIGHT,:SECTOR_SIZE].T
 SH = SECTOR_GRID.shape
 SECTOR_GRID = SECTOR_GRID.reshape((SH[0]*SH[1]*SH[2],3))
 
-class SectorNoise2D(object):
-    def __init__(self, seed, step = SECTOR_SIZE, step_offset = 0, scale = 1, offset = 0):
-        self.noise = noise.SimplexNoise(seed = seed)
-        self.seed = seed
-        self.step = step
-        self.scale = scale
-        self.offset = offset
-        Z = numpy.mgrid[0:SECTOR_SIZE,0:SECTOR_SIZE].T
-        shape = Z.shape
-        self.Z = Z.reshape((shape[0]*shape[1],2))+step_offset
-
-    def __call__(self, position):
-        Z = self.Z + numpy.array([position[0],position[2]])
-        N=self.noise.noise(Z/self.step)*self.scale + self.offset
-        return N.reshape((SECTOR_SIZE,SECTOR_SIZE))
-
-def _initialize_map_generator():
-    global noise1, noise2, noise3, noise4
-    HILL_STEP = 40.0
-    HILL_SCALE = 5
-    HILL_OFFSET = 5
-    CONTINENTAL_STEP = 1500.0
-    CONTINENTAL_SCALE = 40.0
-    CONTINENTAL_OFFSET = 20
-    GAIN_STEP = 3000.0
-    GAIN_SCALE = 5
-    GAIN_OFFSET = 5
-    seed = int(time.time())
-    noise1 = SectorNoise2D(seed = seed+12, step = HILL_STEP, step_offset = 30,  
-        scale = HILL_SCALE, offset = HILL_OFFSET)
-    noise2 = SectorNoise2D(seed = seed+16, step = HILL_STEP, step_offset = 900,
-        scale = HILL_SCALE, offset = HILL_OFFSET)
-    noise3 = SectorNoise2D(seed = seed+14, step = CONTINENTAL_STEP, step_offset = 531,
-        scale = CONTINENTAL_SCALE, offset = CONTINENTAL_OFFSET)
-    noise4 = SectorNoise2D(seed = seed+18, step = GAIN_STEP, step_offset = 8123,
-        scale = GAIN_SCALE, offset = GAIN_OFFSET)
 
 class Sector(object):
     def __init__(self,position,group,model,shown=True):
@@ -277,24 +241,7 @@ class Sector(object):
         simplex noise.
 
         """
-        STONE = BLOCK_ID['Stone']
-        SAND = BLOCK_ID['Sand']
-        GRASS = BLOCK_ID['Grass']
-
-        N1=noise1(self.position)
-        N2=noise2(self.position)
-        N3=noise3(self.position)
-        N4=noise4(self.position)
-
-        N1 = N1*N4+N3
-        N2 = N2+N3
-
-        b = numpy.zeros((SECTOR_HEIGHT,SECTOR_SIZE,SECTOR_SIZE),dtype='u2')
-        for y in range(SECTOR_HEIGHT):
-            b[y] = ((y-40<N1-3)*STONE + (((y-40>=N1-3) & (y-40<N1))*GRASS))
-            thresh = ((y-40>N3)*(y-40<N2)*(y>10))>0
-            b[y] = b[y]*(1 - thresh) + SAND * thresh
-        self.blocks = b.swapaxes(0,1).swapaxes(0,2)
+        self.blocks = mapgen.generate_sector(self.position, self, self.model)
 
 
 class Model(object):
@@ -304,7 +251,7 @@ class Model(object):
         # A TextureGroup manages an OpenGL texture.
         self.group = TextureGroup(image.load(TEXTURE_PATH).get_texture())
         
-        _initialize_map_generator()
+        mapgen.initialize_map_generator()
 
         # The world is stored in sector chunks.
         self.sectors = {}
