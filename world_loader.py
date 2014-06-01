9fcd3340267190ef75d8c8b6f0132d2202595532
Damien Moore <damienlmoore@gmail.com>
2014-06-01 02:17:48 -0400

    1. basic (but untested) implementation of multi-player server (still needs client side implementation) 2. move SectorLoader to world_loader module and go back to using it by default

--- world_loader.py
+++ world_loader.py
@@ -24,6 +24,133 @@ SECTOR_GRID = numpy.mgrid[:SECTOR_SIZE,:SECTOR_HEIGHT,:SECTOR_SIZE].T
 SH = SECTOR_GRID.shape
 SECTOR_GRID = SECTOR_GRID.reshape((SH[0]*SH[1]*SH[2],3))
 
+class SectorLoader(object):
+    def __init__(self):
+        self.pipe, self._pipe = multiprocessing.Pipe()
+#        self.blocks = multiprocessing.Array('B',SECTOR_SIZE*SECTOR_SIZE*SECTOR_HEIGHT)
+#        self.v = multiprocessing.Array('f',200000)
+#        self.t = multiprocessing.Array('f',200000)
+#        self.n = multiprocessing.Array('f',200000)
+#        self.c = multiprocessing.Array('f',200000)
+        self.blocks = numpy.zeros((SECTOR_SIZE+2,SECTOR_HEIGHT,SECTOR_SIZE+2),dtype='u2')
+        self.vt_data = None
+
+#        self.count = multiprocessing.Value('f')
+        self.process = multiprocessing.Process(target = self._loader)
+        self.process.start()
+
+    def _loader(self):
+        print('loader started')
+        mapgen.initialize_map_generator()
+        while True:
+            try:
+                msg, data = self._pipe.recv()
+                print('received', msg)
+            except:
+                return
+            if msg == 'quit':
+                print('loader terminated by owner')
+                return
+            if msg == 'request_sector':
+                pos = (data[0], 0, data[2])
+                self._initialize(pos)
+                self._calc_vertex_data(pos)
+                self._pipe.send_bytes(cPickle.dumps([pos, self.blocks, self.vt_data],-1))
+            if msg == 'add_block':
+                spos = data[0]
+                pos = data[1]
+                block_id = data[2]
+                self.blocks = data[3]
+                self.set_block(pos, spos, block_id)
+                self._calc_vertex_data(spos)
+                self._pipe.send_bytes(cPickle.dumps([spos, self.blocks, self.vt_data],-1))
+            if msg == 'remove_block':
+                spos = data[0]
+                pos = data[1]
+                self.blocks = data[2]
+                self.set_block(pos, spos, 0)
+                self._calc_vertex_data(spos)
+                self._pipe.send_bytes(cPickle.dumps([spos, self.blocks, self.vt_data],-1))
+
+    def poll(self):
+        return self.pipe.poll()
+
+    def send(self, list_object):
+        self.pipe.send(list_object)
+
+    def send_bytes(self, bytes_object):
+        self.pipe.send_bytes(bytes_object)
+
+    def recv(self):
+        return self.pipe.recv()
+
+    def recv_bytes(self):
+        return self.pipe.recv_bytes()
+
+    def _initialize(self, position):
+        """ Initialize the sector by procedurally generating terrain using
+        simplex noise.
+
+        """
+        self.blocks = mapgen.generate_sector(position, None, None)
+
+    def _calc_exposed_faces(self):
+        #TODO: The 3D bitwise ops are slow
+        air = BLOCK_SOLID[self.blocks] == 0
+
+        light = numpy.cumproduct(air[:,::-1,:], axis=1)[:,::-1,:]
+
+        exposed = numpy.zeros(air.shape,dtype=numpy.uint8)
+        exposed[:,:-1,:] |= air[:,1:,:]<<7 #up
+        exposed[:,1:,:] |= air[:,:-1,:]<<6 #down
+        exposed[1:,:,:] |= air[:-1,:,:]<<5 #left
+        exposed[:-1,:,:] |= air[1:,:,:]<<4 #right
+        exposed[:,:,:-1] |= air[:,:,1:]<<3 #forward
+        exposed[:,:,1:] |= air[:,:,:-1]<<2 #back
+        self.exposed = exposed*(~air)
+
+    def _calc_vertex_data(self,position):
+        self._calc_exposed_faces()
+        exposed = self.exposed[1:-1,:,1:-1]
+        sh = exposed.shape
+        exposed = exposed.swapaxes(0,2).reshape(sh[0]*sh[1]*sh[2])
+        egz = exposed>0
+        pos = SECTOR_GRID[egz] + position
+        exposed = exposed[egz]
+        exposed = numpy.unpackbits(exposed[:,numpy.newaxis],axis=1)
+        exposed = numpy.array(exposed,dtype=bool)
+        exposed = exposed[:,:6]
+        #b = self[pos]
+        p = (pos - numpy.array(position)).T
+        b = self.blocks[1:-1,:,1:-1][p[0],p[1],p[2]]
+        texture_data = BLOCK_TEXTURES[b]
+        color_data = BLOCK_COLORS[b]
+        normal_data = numpy.tile(BLOCK_NORMALS,(len(b),1,4))#*exposed_light[:,:,numpy.newaxis]
+        vertex_data = 0.5*BLOCK_VERTICES[b] + numpy.tile(pos,4)[:,numpy.newaxis,:]
+
+        v = vertex_data[exposed].ravel()
+        t = texture_data[exposed].ravel()
+        n = normal_data[exposed].ravel()
+        c = color_data[exposed].ravel()
+        count = len(v)/3
+
+        self.vt_data = (count, v, t, n, c)
+
+    def get_block(self, position, sector_position):
+        pos = position - numpy.array(sector_position)
+        if len(pos.shape)>1:
+            pos = pos.T
+            return self.blocks[1:-1,:,1:-1][pos[0],pos[1],pos[2]]
+        return self.blocks[1:-1,:,1:-1][pos[0],pos[1],pos[2]]
+
+    def set_block(self, position, sector_position, val):
+        pos = position - numpy.array(sector_position)
+        if len(pos.shape)>1:
+            pos = pos.T
+            self.blocks[1:-1,:,1:-1][pos[0],pos[1],pos[2]] = val
+        self.blocks[1:-1,:,1:-1][pos[0],pos[1],pos[2]] = val
+
+
 class Sector(object):
     def __init__(self,position,model):
         self.position = position[0],0,position[2]
@@ -308,7 +435,7 @@ class Loader(object):
         print('starting loader at %s:%i'%(LOADER_IP,LOADER_PORT))
         self.listener = multiprocessing.connection.Listener(address = (LOADER_IP,LOADER_PORT), authkey = 'password')
         if server_ip is not None:
-            print 'Connecting to multiplayer server',server_ip,':',SERVER_PORT
+            print('Connecting to multiplayer server',server_ip,':',SERVER_PORT)
             self.mp_server = multiprocessing.connection.Client(address = (server_ip, SERVER_PORT), authkey = 'password')
         else:
             self.mp_server = None
@@ -357,7 +484,7 @@ class Loader(object):
                         self.connections.remove(conn)
             if self.mp_server is not None and self.mp_server in r:
                 msg,data = conn.recv()
-                print 'msg',data
+                print('msg',data)
             if accept_new:
                 conn = self.accept_connection()
                 print('accept',conn)
--- world_proxy.py
+++ world_proxy.py
@@ -7,6 +7,9 @@ import multiprocessing.connection
 import multiprocessing.sharedctypes
 import multiprocessing
 import cPickle
+import gc
+
+#gc.set_debug(gc.DEBUG_LEAK)
 
 # pyglet imports
 import pyglet
@@ -21,136 +24,6 @@ from config import SECTOR_SIZE, SECTOR_HEIGHT, LOADED_SECTORS
 from util import normalize, sectorize, FACES, cube_v, cube_v2
 from blocks import BLOCK_VERTICES, BLOCK_COLORS, BLOCK_NORMALS, BLOCK_TEXTURES, BLOCK_ID, BLOCK_SOLID, TEXTURE_PATH
 import mapgen
-
-SECTOR_GRID = numpy.mgrid[:SECTOR_SIZE,:SECTOR_HEIGHT,:SECTOR_SIZE].T
-SH = SECTOR_GRID.shape
-SECTOR_GRID = SECTOR_GRID.reshape((SH[0]*SH[1]*SH[2],3))
-
-class SectorLoader(object):
-    def __init__(self):
-        self.pipe, self._pipe = multiprocessing.Pipe()
-#        self.blocks = multiprocessing.Array('B',SECTOR_SIZE*SECTOR_SIZE*SECTOR_HEIGHT)
-#        self.v = multiprocessing.Array('f',200000)
-#        self.t = multiprocessing.Array('f',200000)
-#        self.n = multiprocessing.Array('f',200000)
-#        self.c = multiprocessing.Array('f',200000)
-        self.blocks = numpy.zeros((SECTOR_SIZE+2,SECTOR_HEIGHT,SECTOR_SIZE+2),dtype='u2')
-        self.vt_data = None
-
-#        self.count = multiprocessing.Value('f')
-        self.process = multiprocessing.Process(target = self._loader)
-        self.process.start()
-
-    def _loader(self):
-        print('loader started')
-        mapgen.initialize_map_generator()
-        while True:
-            try:
-                msg, data = self._pipe.recv()
-                print('received', msg)
-            except:
-                return
-            if msg == 'quit':
-                print('loader terminated by owner')
-                return
-            if msg == 'request_sector':
-                pos = (data[0], 0, data[2])
-                self._initialize(pos)
-                self._calc_vertex_data(pos)
-                self._pipe.send_bytes(cPickle.dumps([pos, self.blocks, self.vt_data],-1))
-            if msg == 'add_block':
-                spos = data[0]
-                pos = data[1]
-                block_id = data[2]
-                self.blocks = data[3]
-                self.set_block(pos, spos, block_id)
-                self._calc_vertex_data(spos)
-                self._pipe.send_bytes(cPickle.dumps([spos, self.blocks, self.vt_data],-1))
-            if msg == 'remove_block':
-                spos = data[0]
-                pos = data[1]
-                self.blocks = data[2]
-                self.set_block(pos, spos, 0)
-                self._calc_vertex_data(spos)
-                self._pipe.send_bytes(cPickle.dumps([spos, self.blocks, self.vt_data],-1))
-
-    def poll(self):
-        return self.pipe.poll()
-
-    def send(self, list_object):
-        self.pipe.send(list_object)
-
-    def send_bytes(self, bytes_object):
-        self.pipe.send_bytes(bytes_object)
-
-    def recv(self):
-        return self.pipe.recv()
-
-    def recv_bytes(self):
-        return self.pipe.recv_bytes()
-
-    def _initialize(self, position):
-        """ Initialize the sector by procedurally generating terrain using
-        simplex noise.
-
-        """
-        self.blocks = mapgen.generate_sector(position, None, None)
-
-    def _calc_exposed_faces(self):
-        #TODO: The 3D bitwise ops are slow
-        air = BLOCK_SOLID[self.blocks] == 0
-
-        light = numpy.cumproduct(air[:,::-1,:], axis=1)[:,::-1,:]
-
-        exposed = numpy.zeros(air.shape,dtype=numpy.uint8)
-        exposed[:,:-1,:] |= air[:,1:,:]<<7 #up
-        exposed[:,1:,:] |= air[:,:-1,:]<<6 #down
-        exposed[1:,:,:] |= air[:-1,:,:]<<5 #left
-        exposed[:-1,:,:] |= air[1:,:,:]<<4 #right
-        exposed[:,:,:-1] |= air[:,:,1:]<<3 #forward
-        exposed[:,:,1:] |= air[:,:,:-1]<<2 #back
-        self.exposed = exposed*(~air)
-
-    def _calc_vertex_data(self,position):
-        self._calc_exposed_faces()
-        exposed = self.exposed[1:-1,:,1:-1]
-        sh = exposed.shape
-        exposed = exposed.swapaxes(0,2).reshape(sh[0]*sh[1]*sh[2])
-        egz = exposed>0
-        pos = SECTOR_GRID[egz] + position
-        exposed = exposed[egz]
-        exposed = numpy.unpackbits(exposed[:,numpy.newaxis],axis=1)
-        exposed = numpy.array(exposed,dtype=bool)
-        exposed = exposed[:,:6]
-        #b = self[pos]
-        p = (pos - numpy.array(position)).T
-        b = self.blocks[1:-1,:,1:-1][p[0],p[1],p[2]]
-        texture_data = BLOCK_TEXTURES[b]
-        color_data = BLOCK_COLORS[b]
-        normal_data = numpy.tile(BLOCK_NORMALS,(len(b),1,4))#*exposed_light[:,:,numpy.newaxis]
-        vertex_data = 0.5*BLOCK_VERTICES[b] + numpy.tile(pos,4)[:,numpy.newaxis,:]
-
-        v = vertex_data[exposed].ravel()
-        t = texture_data[exposed].ravel()
-        n = normal_data[exposed].ravel()
-        c = color_data[exposed].ravel()
-        count = len(v)/3
-
-        self.vt_data = (count, v, t, n, c)
-
-    def get_block(self, position, sector_position):
-        pos = position - numpy.array(sector_position)
-        if len(pos.shape)>1:
-            pos = pos.T
-            return self.blocks[1:-1,:,1:-1][pos[0],pos[1],pos[2]]
-        return self.blocks[1:-1,:,1:-1][pos[0],pos[1],pos[2]]
-
-    def set_block(self, position, sector_position, val):
-        pos = position - numpy.array(sector_position)
-        if len(pos.shape)>1:
-            pos = pos.T
-            self.blocks[1:-1,:,1:-1][pos[0],pos[1],pos[2]] = val
-        self.blocks[1:-1,:,1:-1][pos[0],pos[1],pos[2]] = val
 
 
 class SectorProxy(object):
@@ -177,7 +50,6 @@ class SectorProxy(object):
         return draw_invalid
 
     def __getitem__(self, position):
-#        position = normalize(position)
         pos = position - numpy.array(self.bposition)
         if len(pos.shape)>1:
             pos = pos.T
@@ -189,15 +61,16 @@ class SectorProxy(object):
 
     def check_show(self,add_to_batch = True):
         if add_to_batch and self.vt_data is not None:
-            if self.vt is not None:
+            if self.vt != None:
                 print('deleting vt',self.position)
                 self.vt.delete()
             (count, v, t, n, c) = self.vt_data
             self.vt = self.batch.add(count, gl.GL_QUADS, self.group,
-                ('v3f/static', v),
-                ('t2f/static', t),
-                ('n3f/static', n),
-                ('c3B/static', c))
+                ('v3f/static', v.copy()),
+                ('t2f/static', t.copy()),
+                ('n3f/static', n.copy()),
+                ('c3B/static', c.copy()))
+            self.vt_data = None
 
 
 class ModelProxy(object):
@@ -213,15 +86,17 @@ class ModelProxy(object):
         self.update_ref_pos = None
 
         self.loader_requests = []
+        self.active_loader_request = [None, None]
         self.n_requests = 0
         self.n_responses = 0
 
-        world_loader.start_loader(config.SERVER_IP)
-        self.loader = multiprocessing.connection.Client(address = (config.LOADER_IP,config.LOADER_PORT), authkey = 'password')
-#        if config.SERVER_IP == None:
-#            self.loader = SectorLoader()
-#        else:
-#            self.loader = multiprocessing.connection.Client(address = (config.SERVER_IP,config.SERVER_PORT), authkey = 'password')
+        if config.SERVER_IP == None:
+            print('USING LIGHTWEIGHT LOADER')
+            self.loader = world_loader.SectorLoader()
+        else:
+            print('USING MP LOADER')
+            world_loader.start_loader(config.SERVER_IP)
+            self.loader = multiprocessing.connection.Client(address = (config.LOADER_IP,config.LOADER_PORT), authkey = 'password')
 
     def __getitem__(self, position):
         """
@@ -238,22 +113,22 @@ class ModelProxy(object):
             s = self.sectors[spos]
             blocks = s.blocks
             #if position is at edge of block, update neighboring block as well
-            self.loader_requests.insert(0,['add_block',[position, block]])
-#            if config.SERVER_IP is not None:
+            if config.SERVER_IP is not None:
+                self.loader_requests.insert(0,['add_block',[position, block]])
 #                self.loader_requests.insert(0,['add_block',[position, block]])
-#            else:
-#                self.loader_requests.insert(0,['add_block',[spos, position, block, blocks]])
+            else:
+                self.loader_requests.insert(0,['add_block',[spos, position, block, blocks]])
 
     def remove_block(self, position):
         spos = sectorize(position)
         if spos in self.sectors:
             s = self.sectors[spos]
             blocks = s.blocks
-            self.loader_requests.insert(0,['remove_block',[position]])
-#            if config.SERVER_IP is not None:
+            if config.SERVER_IP is not None:
+                self.loader_requests.insert(0,['remove_block',[position]])
 #                self.loader_requests.insert(0,['remove_block',[position]])
-#            else:
-#                self.loader_requests.insert(0,['remove_block',[spos, position, blocks]])
+            else:
+                self.loader_requests.insert(0,['remove_block',[spos, position, blocks]])
 
     def draw(self, position, (center, radius)):
         #t = time.time()
@@ -290,17 +165,29 @@ class ModelProxy(object):
                         + numpy.array([dx*SECTOR_SIZE,dy,dz*SECTOR_SIZE])
                     pos = sectorize(pos)
                     dist = (pos[0]-new[0])**2 + (pos[2]-new[2])**2
-                    if pos not in self.sectors:
+                    if pos not in self.sectors and pos != self.active_loader_request[1]:
                         self.update_sectors_pos.append((dist,pos))
                 for s in list(self.sectors):
-                    if (new[0] - s[0])**2 + (new[2] - s[2])**2 > (LOADED_SECTORS*SECTOR_SIZE)**2:
-                        print('dropping sector',s)
-                        if self.sectors[s].vt:
-                            self.sectors[s].vt.delete()
+                    if abs(new[0] - s[0])>LOADED_SECTORS*SECTOR_SIZE or abs(new[2] - s[2]) > LOADED_SECTORS*SECTOR_SIZE:
+                        print('dropping sector',s,len(self.sectors))
+                        vt = None
+                        if self.sectors[s].vt != None:
+                            vt = self.sectors[s].vt
+#                            self.sectors[s].vt.delete()
+                        del self.sectors[s].vt
+                        del self.sectors[s].vt_data
+                        del self.sectors[s].batch
+                        del self.sectors[s].model
+                        del self.sectors[s].group
                         del self.sectors[s]
+                        if vt:
+                            vt.delete()
+#                        print(gc.collect(1))
+#                        print(gc.garbage)
                 self.update_sectors_pos = sorted(self.update_sectors_pos)
             if len(self.update_sectors_pos)>0:
-                spos = self.update_sectors_pos.pop(0)[1]
+                self.active_loader_request = self.update_sectors_pos.pop(0)
+                spos = self.active_loader_request[1]
                 print('requesting sector',spos)
                 try:
                     req_pos = [r[0] for r in self.loader_requests].index('request_sector') #insert job below higher priority jobs
@@ -316,6 +203,7 @@ class ModelProxy(object):
             try:
                 spos, blocks, vt_data = self.loader.recv()
                 self.n_responses = self.n_requests
+                self.active_loader_request = [None, None]
                 print('recv', spos, len(vt_data[1]))
                 print('took', time.time()-self.loader_time)
                 if spos in self.sectors:
